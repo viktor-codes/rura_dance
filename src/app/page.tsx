eef2f90a76d03e4045be49de4d285af2fcc1943d
Viktor Rura <ruravictor@gmail.com>
2025-09-30 13:05:30 +0100

    Add grain to the bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 import Image from 'next/image'
-
+import grainImage from '@/assets/grain2.jpg'
 export default function Home() {
 	return (
-		<div>
-			<h1 className='font-light font-angst text-5xl uppercase'>
-				rura dance academy
-			</h1>
-			<h1 className='font-normal font-angst text-5xl uppercase'>
-				rura dance academy
-			</h1>
-			<h1 className='font-bold font-angst text-5xl uppercase'>
-				rura dance academy
-			</h1>
-			<p className='text-xl'>This is a sample text using the Inter font.</p>
-		</div>
+		<section className='pt-8 pb-20 md:pt-5 md:pb-10 bg-linear-to-l from-my-primary to-my-primary/80 overflow-x-clip'>
+			<div className=' flex min-h-screen flex-col items-center justify-center p-24'>
+				<div
+					className='absolute inset-0 -z-30 opacity-60'
+					style={{ backgroundImage: `url(${grainImage.src})` }}
+				></div>
+				<h1 className='font-normal tracking-widest font-angst text-6xl text-my-accent uppercase'>
+					typography
+				</h1>
+				<h1 className='font-normal tracking-widest font-angst text-6xl text-my-secondary uppercase'>
+					about us
+				</h1>
+			</div>
+		</section>
 	)
 }
